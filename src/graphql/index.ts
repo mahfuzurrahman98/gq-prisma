676ba5ea84d8bd70baaef5631f9299e5863d4d5a
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-12-16 19:02:53 +0600

    import everything in index file

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,44 +1,20 @@
 import { ApolloServer } from '@apollo/server';
-import { prisma } from '../lib/db';
+import User from './user';
 
 const typeDefs = `
-    type Query {
-      hello: String
-    },
-    type Mutation {
-      createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
-    }
-  `;
+  type Query {
+    ${User.queries}
+  }
+  type Mutation {
+    ${User.mutations}
+  }
+`;
 const resolvers = {
   Query: {
-    hello: () => 'Hello, world!',
+    ...User.resolvers.queries,
   },
   Mutation: {
-    createUser: async (
-      _: any,
-      {
-        firstName,
-        lastName,
-        email,
-        password,
-      }: {
-        firstName: string;
-        lastName: string;
-        email: string;
-        password: string;
-      }
-    ) => {
-      await prisma.user.create({
-        data: {
-          firstName,
-          lastName,
-          email,
-          password,
-          profileImage: '', // Add the profileImage as an empty string
-        },
-      });
-      return true;
-    },
+    ...User.resolvers.mutations,
   },
 };
 
